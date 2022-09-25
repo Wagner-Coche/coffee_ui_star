cfd93912ed7646807e690ffc61b12fab45de6d27
Wagner-Coche <wagnercoche@gmail.com>
2022-09-25 22:48:04 +0100

    configure - hide status bar

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,11 +1,16 @@
 import '../app/presenter/screens/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.immersive
+    );
+
     return MaterialApp(
       title: "Coffee UI",
       debugShowCheckedModeBanner: false,
